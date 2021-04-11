1c2e6180d62f7b656148b2dc7437aeb27e44b3bd
Lizwi <lsilimela23@gmail.com>
2021-04-11 17:26:40 +0200

    project setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        this is the home page
+    </div>
+</div>
+
+@endsection
